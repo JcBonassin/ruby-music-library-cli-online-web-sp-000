26dc45631c920b0d3f98a23b05a858f97fa9fdb0
JC <bonassin@gmail.com>
2021-01-25 17:40:00 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,30 +1,26 @@
 class Genre
 attr_accessor :name, :songs
 
-@@all = []
+attr_accessor :name
 
-def self.all #Class reader
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
     @@all
   end
 
-def initialize
-  save
-end
-
   def self.destroy_all
-  @@all.clear
-end
+    all.clear
+  end
 
-def self.count
-  @@all.size
-end
-
-def save
-  @@all << self
-end
-
-def find_by_name(name)
-    self.all.detect{|artist| artist.name == name}
+  def save
+    self.class.all << self
   end
 
 
