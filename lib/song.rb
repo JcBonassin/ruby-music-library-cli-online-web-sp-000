2383695185780509c9ecca566c895d2dd25fd262
JC <bonassin@gmail.com>
2021-01-25 17:43:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,11 @@
 class Song
   attr_accessor :name
-  attr_reader :artist, :genre
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
-  end
-
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self)
-  end
-
-  def genre=(genre)
-    @genre = genre
-    genre.songs << self unless genre.songs.include?(self)
   end
 
   def self.all
